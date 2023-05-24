8c16ae9819c505b161e1b0138f55763d04c143ac
Parami <paramiashinsana@gmail.com>
2023-05-24 16:57:29 +0530

    LA

---
+++ Layered-Project-main/Layered_Architecture/src/dao/PlaceOrderDAOImpl.java
@@ -0,0 +1,55 @@
+package dao;
+
+public class PlaceOrderDAOImpl {
+
+
+//public void findItem(){
+//
+//}
+//
+//public boolean existItems(){
+//
+//}
+//
+//public boolean existCustomer(){
+//
+//}
+//
+//public String generateNewOrderId(){
+//        String name = nimal;
+//        return name;
+//}
+//public void placeOrderloadAllCustomerIds(){
+//
+//}
+//
+//public void placeOrderloadAllItemCodes(){
+//
+//}
+//
+//public void searchCustomers(){
+//
+//}
+//
+//public String generateNewOrderId(){
+//    String name = nimal;
+//    return name;
+//}
+//
+//public void PlaceOrdersTransaction(){
+//
+//}
+//
+//public void poSearchAndUpdate(){
+//
+//}
+//
+//public void plFindItem(String code) throws SQLException{
+//    Connection connection = DBConnection.getDbConnection().getConnection();
+//    PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Item WHERE code=?");
+//    pstm.setString(1,OrderDTO.getCode);
+//    ResultSet rst = pstm.executeQuery();
+//    rst.next();
+//}
+
+}
